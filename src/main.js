6c8883bd922243b9965ed598a934acbd77a971d9
jsandovalg <jorge@stakeordie.com>
2021-04-12 16:04:19 -0700

    Add Keplr plugin and main components

--- src/main.js
+++ src/main.js
@@ -1,46 +1,9 @@
-import griptape from '@stakeordie/griptape.js';
-import { useStore } from 'vuex';
+import KeplrAccount from './components/keplr/KeplrAccount';
+import KeplrUser from './components/keplr/KeplrUser';
+import Keplr from './plugins/keplr';
 
-export default {
-    install: (app, { store }) => {
-
-        store.registerModule('$griptape', {
-            namespaced: true,
-
-            state: {
-                // The minting inflation.
-                inflation: null
-            },
-
-            getters: {
-                getInflation: state => state.inflation
-            },
-
-            mutations: {
-                updateInflation: (state, inflation) => {
-                    state.inflation = inflation;
-                }
-            },
-
-            actions: {
-                // Update the minting inflation
-                async updateInflation(context) {
-                    const inflation = await griptape.mint.getInflation();
-
-                    // For now we don't need to transform the response in any
-                    // way. Consider to transform it if needed.
-                    context.commit('updateInflation', inflation);
-                }
-            }
-        });
-
-        // griptape entry-point
-        const api = {
-            updateInflation() {
-                store.dispatch('$griptape/updateInflation');
-            }
-        };
-
-        app.provide('$griptape', api);
-    }
+export {
+    KeplrAccount as KeplrAccount,
+    KeplrUser as KeplrUser,
+    Keplr as Keplr
 }
