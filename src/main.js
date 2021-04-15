7a9744318f6bac5500608fdd629966851371407c
jsandovalg <jorge@stakeordie.com>
2021-04-15 15:27:29 -0700

    cancel export LoadingIcon and SecretOverlay components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import KeplrAccount from './components/keplr/KeplrAccount';
 import KeplrUser from './components/keplr/KeplrUser';
-import SecretOverlay from './components/keplr/SecretOverlay'
 
 import VkeysAddress from './components/wallet/VkeysAddress';
 import VkeysWallet from './components/wallet/VkeysWallet';
-import LoadingIcon from './components/wallet/LoadingIcon';
 
 import Keplr from './plugins/keplr';
 import VKeys from './plugins/vkeys';
@@ -13,11 +11,9 @@ import VKeys from './plugins/vkeys';
 export {
     KeplrAccount as KeplrAccount,
     KeplrUser as KeplrUser,
-    SecretOverlay as SecretOverlay,
 
     VkeysAddress as VkeysAddress,
     VkeysWallet as VkeysWallet,
-    LoadingIcon as LoadingIcon,
 
     Keplr as Keplr,
     VKeys as VKeys
